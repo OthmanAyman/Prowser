96f93bbdf13191e3ccb716c4f7fcbc49c4f55f42
OthmanAyman <othman.ayman47@gmail.com>
2020-06-10 01:00:16 +0300

    removed env folder and updated browser.py

--- browser.py
+++ browser.py
@@ -1,29 +1,21 @@
 
 
-import socket 
+import socket
 import requests
 
 
-def request_html_https(url):
-    if url[:7] == 'https://':
-        return requests.get(url).text
-    else:
-        url = 'https://' + url
-        return requests.get(url).text
 
-print(request_html_https("google.com"))
-"""mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('google.com', 80))
 cmd = 'GET http://google.com/ HTTP/1.0\r\n\r\n'.encode()
+
 mysock.send(cmd)
+
+
 while True:
- 
+
     data = mysock.recv(512)
-   
+
     if len(data) < 1:
         break
     print(data.decode(), end=' ')
-
-
-
-"""
