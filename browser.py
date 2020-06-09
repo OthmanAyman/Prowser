79dd0931e55b4c9c155b014149007016d99cdbc0
xziad <lelouch0511@gmail.com>
2020-06-09 23:53:01 +0300

    next time i'll add http support :)

--- browser.py
+++ browser.py
@@ -12,7 +12,7 @@ def request_html_https(url):
         return requests.get(url).text
 
 print(request_html_https("google.com"))
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+"""mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('google.com', 80))
 cmd = 'GET http://google.com/ HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
@@ -26,3 +26,4 @@ while True:
 
 
 
+"""
